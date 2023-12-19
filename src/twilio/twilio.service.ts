9a15aa9e17d6f3ce80b664d753631321cd02c915
ParkBig <enfantgu@gmail.com>
2023-12-19 22:59:50 +0900

    twilio test

--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -27,6 +27,7 @@ export class TwilioService {
   async sendVerification({
     phoneNumber,
   }: SendVerificationInput): Promise<SendVerificationOutput> {
+    console.log(phoneNumber);
     try {
       const korPhoneNumber = phone(phoneNumber, { country: 'KOR' });
       if (!korPhoneNumber.isValid) {
